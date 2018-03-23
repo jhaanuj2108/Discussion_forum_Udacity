7853959926590f69261a66e9b843171ab621736b
jhaanuj2108 <jha.anuj.2108@gmail.com>
2018-03-23 11:41:17 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/disscussionforum/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.android.disscussionforum;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mWebView = findViewById(R.id.myWebView);
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setAppCacheEnabled(true);
+        mWebView.loadUrl("https://www.discussions.udacity.com");
+        mWebView.setWebViewClient(new WebViewClient());
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mWebView.canGoBack()) {
+            mWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
